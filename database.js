815bbdcb95586117fb871c2a62768ed6339a345c
jatin <jatin26pandey@gmail.com>
2023-06-18 11:40:10 +0530

    frontend link updated

--- database.js
+++ database.js
@@ -3,4 +3,5 @@ import mongoose from "mongoose";
 export const connect = async () => {
  const { connection } = await mongoose.connect(process.env.MONGO_URI);
  console.log(`MongoDB connected with ${connection.host}`);
+ 
 };
